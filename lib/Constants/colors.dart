beb444b0e648b7946e4b4df47f0cfa344c0f90cc
santosh <sahusantosh19862@gmail.com>
2025-09-27 02:03:05 +0530

    dasdas

---
+++ lib/Constants/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const tPrimaryColor = Colors.amber;
+const tSecondColor = Color(0X00ffffff);
+const tAccentColor = Colors.blueAccent;
+
+const tWhiteColor = Colors.white;
+const tDarkColor = Color(0X00ffffff);
+const tCardBgColor = Color(0X00ffffff);
+
+const tOnBoardingPage1Color = Color(0X00ffffff);
+const tOnBoardingPage2Color = Color(0X00ffffff);
+const tOnBoardingPage3Color = Color(0X00ffffff);
+
+const String tWelcomeScreen = "assets/images/welcome.jpg";
+const String tWelcomeScreen1 = "assets/images/back0.jpg";
---
+++ lib/Constants/size.dart
@@ -0,0 +1,3 @@
+const tDefaultSize = 30.0;
+const tSplashContianerSize = 30.0;
+const tButtonHeight = 30.0;
