56b3bee3c40b139cdc24679984d3d9c5d88c8431
santosh <sahusantosh19862@gmail.com>
2025-09-27 02:44:54 +0530

    dadasda

---
+++ lib/screens/SplashScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:fluttercollapedrawer/screens/home.dart';
+import 'package:flutter_splash_screen/flutter_splash_screen.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      
+    );
+  }
+}
