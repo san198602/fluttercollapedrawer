64185b5f863fe26fed176a95e3f1313293fe8bd3
santosh <sahusantosh19862@gmail.com>
2025-09-27 05:34:30 +0530

    dasdas

--- lib/main.dart
+++ lib/main.dart
@@ -33,13 +33,15 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) => Scaffold(
     extendBodyBehindAppBar: true,
     drawer: NavDrawer(),
+
     appBar: AppBar(
       backgroundColor: Color.fromRGBO(120, 10, 10, 0.9),
       elevation: 0,
       leading: Builder(
         builder: (BuildContext context) {
           return IconButton(
-            icon: const Icon(Icons.menu),
+            padding: EdgeInsets.all(15.0),
+            icon: Image.asset('assets/images/menu0.png'),
             onPressed: () {
               Scaffold.of(context).openDrawer();
             },
